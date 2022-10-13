865967ed8d89bd05f38b33f39b1d959758b7bd4f
Han <boikoale97@gmail.com>
2022-10-13 20:44:20 +0300

    refactor: offer section
    made new section is offer section and made it responsible but not to finish

---
+++ src/js/files/my-scripts/timer.js
@@ -0,0 +1,20 @@
+const days = document.querySelector(".timer__days > .timer__count");
+const hour = document.querySelector(".timer__hour > .timer__count");
+const minutes = document.querySelector(".timer__minutes > .timer__count");
+const second = document.querySelector(".timer__second > .timer__count");
+
+let counter = Number(second.innerHTML);
+
+window.addEventListener("DOMContentLoaded", () => {
+  second.innerHTML = counter;
+  setInterval(() => {
+    --counter;
+    if (counter === 0) {
+      minutes.innerHTML = Number(minutes.innerHTML - 1);
+      counter = 59;
+      second.innerHTML = counter;
+    } else {
+      second.innerHTML = counter;
+    }
+  }, 1000);
+});
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,11 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+import './my-scripts/my-scripts.modules.js';
+
+
+Array.from(document.querySelectorAll('.navigation__link[data-be="false"]')).forEach((el) => {
+    el.addEventListener("click", (e) => {
+        e.preventDefault();
+    })
+})
